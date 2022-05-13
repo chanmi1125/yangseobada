f379e4bd672fda59d9d4bcf05ed40b7d5ed40e44
SaFiRoLa <102794153+SaFiRoLa@users.noreply.github.com>
2022-05-13 17:53:07 +0900

    Add files via upload

---
+++ 새 폴더/timer.js
@@ -0,0 +1,17 @@
+const timeout = setTimeout(() => {
+  console.log("1초 후에 실행 됩니다.");
+}, 1000);
+
+const interval = setInterval(() => {
+  console.log("1초 마다 실행 됩니다.");
+}, 1000);
+
+const immdeiate = setImmediate(() => {
+  console.log("setImmediate() ~");
+});
+
+console.log("setImmediate보다 먼저 실행됩니다.");
+
+setTimeout(() => {
+  clearInterval(interval);
+}, 3000);
