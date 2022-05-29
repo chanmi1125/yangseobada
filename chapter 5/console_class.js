ebb7ff424a4f2eb096f1d468ab68398a86c04df7
chanmi1125 <102790902+chanmi1125@users.noreply.github.com>
2022-05-29 20:50:04 +0900

    Delete chapter 5 directory

--- chapter 5/console_class.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const {Console} = require('console');
-const output = fs.createWriteStream('./stdout.log');
-const errorOutput = fs.createWriteStream('./stderr.log');
-
-const logger = new Console({stdout: output, stderr: errorOutput});
-const count = 5;
-logger.log('count: %d',count);
-logger.error('count: '+count);
