4d733da5006e8ff69c92e085488dce90f507438c
chanmi1125 <102790902+chanmi1125@users.noreply.github.com>
2022-06-02 13:21:54 +0900

    Delete 10 directory

--- 10/js/sol-2.js
+++
@@ -1,9 +0,0 @@
-    function changeBg() {
-      var x = Math.floor(Math.random() * 256);
-      var y = Math.floor(Math.random() * 256);
-      var z = Math.floor(Math.random() * 256);
-      var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-        document.body.style.background = bgColor;
-    }
-
-    changeBg();
